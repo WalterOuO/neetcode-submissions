9955f1018e623842026e011e6314785285fd6054
WalterOuO <150639709+WalterOuO@users.noreply.github.com>
2026-08-17 17:57:26 +0800

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # Memorization + Recursive solution
+        # Initialize: set the way to decode last digit as 1
+        dp = { len(s) : 1}
+
+        def dfs(i):
+            # base case: if i is end of string
+            if i in dp:
+                return dp[i]
+            # bad base case: a single 0 cannot decode to any ch
+            if s[i] == "0":
+                return 0
+
+            # recursivly count
+            res = dfs(i + 1)
+            if (i + 1 < len(s) and (s[i] == '1')):
+                res += dfs(i + 2)
+            if (i + 1 < len(s) and (s[i] == '2') and ('0' <= s[i+1] <= '6')):
+                res += dfs(i + 2)
+            # store the number of way to dictionary
+            dp[i] = res
+            return res
+
+        return dfs(0)
