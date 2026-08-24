15c9c8dff4d85fc2f279d4d7e583cc49d14f96eb
WalterOuO <150639709+WalterOuO@users.noreply.github.com>
2026-08-24 15:33:47 +0800

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        # i & 1 can get the last bit
+        # res | i can add bit into res
+
+        for i in range(32):
+            # as for loop going, get different bit of n
+            getbit = (n >> i) & 1
+            res = res | (getbit << (31-i)) # leftshift to add bit into res
+        return res
+
+        # Evaluation
+        ## Time: O(1)
+        ## Space: O(1)
