a9f63c266607759cce01c339f1e373fe9cf9f4bb
WalterOuO <150639709+WalterOuO@users.noreply.github.com>
2026-08-24 14:41:26 +0800

    Add: counting-bits - submission-5

---
+++ Data Structures & Algorithms/counting-bits/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        # dp : 2 power num [1, 2, 4, 8, 16...]
+        # when n = 2 power num: looks like 1000000 (only one 1)
+        # offset of dp should be 2 power num
+        dp = [0]* (n+1)
+        twopowernum = 1
+        
+        for i in range(1, n+1):
+            if i == 2*twopowernum:
+                twopowernum = i
+            dp[i] = 1 + dp[i-twopowernum]
+
+        return dp
+
+        
