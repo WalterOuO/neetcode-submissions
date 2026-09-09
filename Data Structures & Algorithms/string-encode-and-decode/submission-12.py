08263f6e69a05cdd239c7b30b90f374d876c74ba
WalterOuO <150639709+WalterOuO@users.noreply.github.com>
2026-09-09 17:15:54 +0800

    Add: string-encode-and-decode - submission-12

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-12.py
@@ -0,0 +1,27 @@
+class Solution:
+    # Will the s in strs always begin with capital character? not sure 
+    def encode(self, strs: List[str]) -> str:
+        encoded_s = []
+        for s in strs:
+            encoded_s.append(str(len(s)) + "#" + s) 
+        # 5#Hello5#World
+        return "".join(encoded_s)
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            
+            length = int(s[i:j])    
+            str_start = j + 1
+            str_end = str_start + length
+            res.append(s[str_start:str_end])
+            i = str_end
+        return res
+
+        # assume len(strs) == n, len( longest s in strs) == m
+        # Time: encode O(n) + while loop O(n)
+        # Space: res O(n+m)
